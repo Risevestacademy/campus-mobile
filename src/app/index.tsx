108e2232885a936f071515659753e831119293ad
Ayobami Oludemi <oludemiayobami@gmail.com>
2026-09-23 23:57:05 +0100

    chore: add color tokens and typography

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,9 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white dark:bg-black">
-      <Text className="">Rise Campus</Text>
-    </SafeAreaView>
+    <View className="flex-1 items-center justify-center bg-bg-canvas">
+      <Text className="font-display text-text-primary">Rise Campus</Text>
+    </View>
   );
 }
