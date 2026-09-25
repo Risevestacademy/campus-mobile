3688ed27fc69e1624ac84854ae26735712dced6e
Ayobami Oludemi <oludemiayobami@gmail.com>
2026-09-25 19:25:45 +0100

    chore: atomize text to allow re-use

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import { Text } from "@shared/components/atoms";
+import { View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-bg-canvas">
-      <Text className="font-display text-text-primary">Rise Campus</Text>
+      <Text variant="display">Rise Campus</Text>
     </View>
   );
 }
